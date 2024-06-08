9d2893c480351cc5cff02c7a03fd6332a6dbf627
Abraham <kashuaabraham@yahoo.com>
2024-06-08 12:19:03 +0100

    updated app content

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello, Abraham Kashua!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
